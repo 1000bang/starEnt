505a5f11fb76aec285b68fcf9e0b159bbede4eda
binstarr <tjdqls2s@naver.com>
2022-12-30 15:07:54 +0900

    setting

--- src/main/java/com/threebee/starentertainment/controller/UserController.java
+++ src/main/java/com/threebee/starentertainment/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.threebee.starentertainment.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 	
-	@GetMapping("index")
+	@GetMapping({ "", "/", "index" })
 	public String index() {
 		return "index";
 	}
